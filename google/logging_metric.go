5217cd815f8a080064c1319ab11f6f46145dfbc2
megan07 <mbang@hashicorp.com>
2019-11-11 19:02:33 +0000

    Old generated base as of 48c1244acafb360cbf4520efbfe591d8ca0d426e.

--- google/logging_metric.go
+++ google/logging_metric.go
@@ -134,13 +134,6 @@ func expandLoggingMetricMetricDescriptor(v interface{}, d TerraformResourceData,
 		transformed["labels"] = transformedLabels
 	}
 
-	transformedDisplayName, err := expandLoggingMetricMetricDescriptorDisplayName(original["display_name"], d, config)
-	if err != nil {
-		return nil, err
-	} else if val := reflect.ValueOf(transformedDisplayName); val.IsValid() && !isEmptyValue(val) {
-		transformed["displayName"] = transformedDisplayName
-	}
-
 	return transformed, nil
 }
 
@@ -201,10 +194,6 @@ func expandLoggingMetricMetricDescriptorLabelsDescription(v interface{}, d Terra
 }
 
 func expandLoggingMetricMetricDescriptorLabelsValueType(v interface{}, d TerraformResourceData, config *Config) (interface{}, error) {
-	return v, nil
-}
-
-func expandLoggingMetricMetricDescriptorDisplayName(v interface{}, d TerraformResourceData, config *Config) (interface{}, error) {
 	return v, nil
 }
 
