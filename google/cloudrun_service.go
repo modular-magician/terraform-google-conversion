73401a4837274d896e41e0a749892db9793d9187
Modular Magician <magic-modules@google.com>
2021-06-11 18:28:36 +0000

    New generated code for MM PR 4866.
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google/cloudrun_service.go
+++ google/cloudrun_service.go
@@ -631,7 +631,6 @@ func expandCloudRunServiceSpecTemplateSpecContainersCommand(v interface{}, d Ter
 }
 
 func expandCloudRunServiceSpecTemplateSpecContainersEnv(v interface{}, d TerraformResourceData, config *Config) (interface{}, error) {
-	v = v.(*schema.Set).List()
 	l := v.([]interface{})
 	req := make([]interface{}, 0, len(l))
 	for _, raw := range l {
