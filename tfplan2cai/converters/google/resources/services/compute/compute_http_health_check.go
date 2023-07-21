f524b8ef6d8a8eefffcb9853ed2c000055f296d4
Modular Magician <magic-modules@google.com>
2023-07-21 20:20:37 +0000

    New generated code for MM PR 8395.
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- tfplan2cai/converters/google/resources/services/compute/compute_http_health_check.go
+++ tfplan2cai/converters/google/resources/services/compute/compute_http_health_check.go
@@ -40,8 +40,8 @@ func GetComputeHttpHealthCheckCaiObject(d tpgresource.TerraformResourceData, con
 			Name: name,
 			Type: ComputeHttpHealthCheckAssetType,
 			Resource: &tpgresource.AssetResource{
-				Version:              "beta",
-				DiscoveryDocumentURI: "https://www.googleapis.com/discovery/v1/apis/compute/beta/rest",
+				Version:              "v1",
+				DiscoveryDocumentURI: "https://www.googleapis.com/discovery/v1/apis/compute/v1/rest",
 				DiscoveryName:        "HttpHealthCheck",
 				Data:                 obj,
 			},
