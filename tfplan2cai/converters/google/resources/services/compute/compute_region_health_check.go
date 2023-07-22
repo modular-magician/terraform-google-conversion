95dbccef7bdc7c5590331fa767b62f3af331b000
Modular Magician <magic-modules@google.com>
2023-07-22 00:58:02 +0000

    Old generated code for MM PR 8290.
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- tfplan2cai/converters/google/resources/services/compute/compute_region_health_check.go
+++ tfplan2cai/converters/google/resources/services/compute/compute_region_health_check.go
@@ -41,8 +41,8 @@ func GetComputeRegionHealthCheckCaiObject(d tpgresource.TerraformResourceData, c
 			Name: name,
 			Type: ComputeRegionHealthCheckAssetType,
 			Resource: &tpgresource.AssetResource{
-				Version:              "beta",
-				DiscoveryDocumentURI: "https://www.googleapis.com/discovery/v1/apis/compute/beta/rest",
+				Version:              "v1",
+				DiscoveryDocumentURI: "https://www.googleapis.com/discovery/v1/apis/compute/v1/rest",
 				DiscoveryName:        "RegionHealthCheck",
 				Data:                 obj,
 			},
