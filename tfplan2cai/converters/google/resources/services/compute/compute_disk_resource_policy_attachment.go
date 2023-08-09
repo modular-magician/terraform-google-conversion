4d29e9f7452542ff243fba8d8749d0d07a098661
Modular Magician <magic-modules@google.com>
2023-08-09 21:32:34 +0000

    New generated code for MM PR 8265.
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- tfplan2cai/converters/google/resources/services/compute/compute_disk_resource_policy_attachment.go
+++ tfplan2cai/converters/google/resources/services/compute/compute_disk_resource_policy_attachment.go
@@ -41,8 +41,8 @@ func GetComputeDiskResourcePolicyAttachmentCaiObject(d tpgresource.TerraformReso
 			Name: name,
 			Type: ComputeDiskResourcePolicyAttachmentAssetType,
 			Resource: &tpgresource.AssetResource{
-				Version:              "beta",
-				DiscoveryDocumentURI: "https://www.googleapis.com/discovery/v1/apis/compute/beta/rest",
+				Version:              "v1",
+				DiscoveryDocumentURI: "https://www.googleapis.com/discovery/v1/apis/compute/v1/rest",
 				DiscoveryName:        "DiskResourcePolicyAttachment",
 				Data:                 obj,
 			},
--- tfplan2cai/converters/google/resources/services/compute/compute_node_template.go
+++ tfplan2cai/converters/google/resources/services/compute/compute_node_template.go
@@ -41,8 +41,8 @@ func GetComputeNodeTemplateCaiObject(d tpgresource.TerraformResourceData, config
 			Name: name,
 			Type: ComputeNodeTemplateAssetType,
 			Resource: &tpgresource.AssetResource{
-				Version:              "beta",
-				DiscoveryDocumentURI: "https://www.googleapis.com/discovery/v1/apis/compute/beta/rest",
+				Version:              "v1",
+				DiscoveryDocumentURI: "https://www.googleapis.com/discovery/v1/apis/compute/v1/rest",
 				DiscoveryName:        "NodeTemplate",
 				Data:                 obj,
 			},
