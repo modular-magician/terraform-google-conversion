3eb631e0e216c4035f522fb9672ee19fdc4cf417
Modular Magician <magic-modules@google.com>
2023-08-14 23:32:28 +0000

    New generated code for MM PR 8336.
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- tfplan2cai/converters/google/resources/services/compute/compute_operation.go
+++ tfplan2cai/converters/google/resources/services/compute/compute_operation.go
@@ -3,7 +3,6 @@ package compute
 import (
 	"bytes"
 	"context"
-	"encoding/json"
 	"errors"
 	"fmt"
 	"io"
@@ -13,7 +12,7 @@ import (
 	"github.com/GoogleCloudPlatform/terraform-google-conversion/v2/tfplan2cai/converters/google/resources/tpgresource"
 	transport_tpg "github.com/GoogleCloudPlatform/terraform-google-conversion/v2/tfplan2cai/converters/google/resources/transport"
 
-	compute "google.golang.org/api/compute/v0.beta"
+	"google.golang.org/api/compute/v1"
 )
 
 type ComputeOperationWaiter struct {
@@ -21,7 +20,6 @@ type ComputeOperationWaiter struct {
 	Op      *compute.Operation
 	Context context.Context
 	Project string
-	Parent  string
 }
 
 func (w *ComputeOperationWaiter) State() string {
@@ -78,8 +76,6 @@ func (w *ComputeOperationWaiter) QueryOp() (interface{}, error) {
 	} else if w.Op.Region != "" {
 		region := tpgresource.GetResourceNameFromSelfLink(w.Op.Region)
 		return w.Service.RegionOperations.Get(w.Project, region, w.Op.Name).Do()
-	} else if w.Parent != "" {
-		return w.Service.GlobalOrganizationOperations.Get(w.Op.Name).ParentId(w.Parent).Do()
 	}
 	return w.Service.GlobalOperations.Get(w.Project, w.Op.Name).Do()
 }
@@ -118,32 +114,6 @@ func ComputeOperationWaitTime(config *transport_tpg.Config, res interface{}, pro
 		return err
 	}
 	return tpgresource.OperationWait(w, activity, timeout, config.PollInterval)
-}
-
-func ComputeOrgOperationWaitTimeWithResponse(config *transport_tpg.Config, res interface{}, response *map[string]interface{}, parent, activity, userAgent string, timeout time.Duration) error {
-	op := &compute.Operation{}
-	err := tpgresource.Convert(res, op)
-	if err != nil {
-		return err
-	}
-
-	w := &ComputeOperationWaiter{
-		Service: config.NewComputeClient(userAgent),
-		Op:      op,
-		Parent:  parent,
-	}
-
-	if err := w.SetOp(op); err != nil {
-		return err
-	}
-	if err := tpgresource.OperationWait(w, activity, timeout, config.PollInterval); err != nil {
-		return err
-	}
-	e, err := json.Marshal(w.Op)
-	if err != nil {
-		return err
-	}
-	return json.Unmarshal(e, response)
 }
 
 // ComputeOperationError wraps compute.OperationError and implements the
