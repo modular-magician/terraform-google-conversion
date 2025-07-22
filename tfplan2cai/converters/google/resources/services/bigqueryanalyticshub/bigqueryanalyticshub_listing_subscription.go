1e5f13315a285a3572b4a490baf11bd2c4eb72c8
Modular Magician <magic-modules@google.com>
2025-07-22 10:33:44 +0000

    New generated code for MM PR 14481.
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- tfplan2cai/converters/google/resources/services/bigqueryanalyticshub/bigqueryanalyticshub_listing_subscription.go
+++ tfplan2cai/converters/google/resources/services/bigqueryanalyticshub/bigqueryanalyticshub_listing_subscription.go
@@ -19,6 +19,8 @@ package bigqueryanalyticshub
 import (
 	"reflect"
 
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+
 	"github.com/GoogleCloudPlatform/terraform-google-conversion/v6/tfplan2cai/converters/google/resources/cai"
 	"github.com/hashicorp/terraform-provider-google-beta/google-beta/tpgresource"
 	transport_tpg "github.com/hashicorp/terraform-provider-google-beta/google-beta/transport"
@@ -110,6 +112,13 @@ func expandBigqueryAnalyticsHubListingSubscriptionDestinationDataset(v interface
 		transformed["labels"] = transformedLabels
 	}
 
+	transformedReplicaLocations, err := expandBigqueryAnalyticsHubListingSubscriptionDestinationDatasetReplicaLocations(original["replica_locations"], d, config)
+	if err != nil {
+		return nil, err
+	} else if val := reflect.ValueOf(transformedReplicaLocations); val.IsValid() && !tpgresource.IsEmptyValue(val) {
+		transformed["replicaLocations"] = transformedReplicaLocations
+	}
+
 	return transformed, nil
 }
 
@@ -168,4 +177,9 @@ func expandBigqueryAnalyticsHubListingSubscriptionDestinationDatasetLabels(v int
 		m[k] = val.(string)
 	}
 	return m, nil
+}
+
+func expandBigqueryAnalyticsHubListingSubscriptionDestinationDatasetReplicaLocations(v interface{}, d tpgresource.TerraformResourceData, config *transport_tpg.Config) (interface{}, error) {
+	v = v.(*schema.Set).List()
+	return v, nil
 }
