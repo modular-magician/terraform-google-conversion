21bb1a584e4a4c19eea538572af091c81116903b
Modular Magician <magic-modules@google.com>
2025-09-26 15:16:58 +0000

    New generated code for MM PR 15234.
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- pkg/cai2hcl/converters/resource_converters.go
+++ pkg/cai2hcl/converters/resource_converters.go
@@ -159,7 +159,8 @@ var ConverterMap = map[string]map[string]models.Cai2hclConverter{
 		"Default": compute.NewComputeGlobalAddressCai2hclConverter(provider),
 	},
 	compute.ComputeHealthCheckAssetType: {
-		"Default": compute.NewComputeHealthCheckCai2hclConverter(provider),
+		"ComputeHealthCheck":       compute.NewComputeHealthCheckCai2hclConverter(provider),
+		"ComputeRegionHealthCheck": compute.NewComputeRegionHealthCheckCai2hclConverter(provider),
 	},
 	compute.ComputeImageAssetType: {
 		"Default": compute.NewComputeImageCai2hclConverter(provider),
