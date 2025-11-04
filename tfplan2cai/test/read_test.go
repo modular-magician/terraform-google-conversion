09c2b4d90cea6754bf4cf948759b676512b7f478
Modular Magician <magic-modules@google.com>
2025-11-04 21:15:20 +0000

    New generated code for MM PR 15609.
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- tfplan2cai/test/read_test.go
+++ tfplan2cai/test/read_test.go
@@ -176,6 +176,7 @@ func TestReadPlannedAssetsCoverage(t *testing.T) {
 		{name: "full_spanner_instance"},
 		{name: "full_sql_database_instance"},
 		{name: "full_storage_bucket"},
+		{name: "google_iam_workload_identity_pool_provider"},
 		{name: "instance"},
 		{name: "logging_organization_sink"},
 		{name: "sql"},
